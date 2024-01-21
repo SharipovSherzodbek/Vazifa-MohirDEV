88423c91ebf791ee4a48048fbe6f6956002d3312
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-01-21 20:51:19 +0500

    CORRECTED

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-System.Console.WriteLine(" \t Homework");
+System.Console.WriteLine(" \t Homework1");
 System.Console.WriteLine("Enter a number to calculate Sum of odd number... ");
 int sumOFOddNumbers = 0 ;
 int number = int.Parse(Console.ReadLine());
@@ -15,16 +15,16 @@ while  (i <= number)
 System.Console.WriteLine("Sum of odd numbers is : " +sumOFOddNumbers);
 
 
-System.Console.WriteLine(" \t Homework 2");
+System.Console.WriteLine(" \n \t Homework 2");
   int[] myArray = { -2, 3, -5, 334, 45, 432, 556, 32, 34, 43, 343, 4, 35, 644, 32, 8, };
   int arrayMax = myArray.Max();
-  int arrayMin = myArray.Min();5
+  int arrayMin = myArray.Min();
   
   int minMax = arrayMax * arrayMin;
-  Console.WriteLine("Multiplying the max and min values ​​of the arrays is equal to " +minMax);
+  Console.WriteLine($"Multiplying the max and min values of the arrays is equal to {arrayMax} x {arrayMin} = {arrayMax*arrayMin}");
 
 
-System.Console.WriteLine(" \t Homework 3");
+System.Console.WriteLine(" \n \t Homework 3");
 System.Console.WriteLine("Enter a number to calculate factorial ...");
 int x = int.Parse(Console.ReadLine());
 long factorial = 1;
