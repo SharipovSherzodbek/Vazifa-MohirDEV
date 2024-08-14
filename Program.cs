a26a0dac494ab4a64beb22ee3e52c7ffd7a582f5
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-08-14 12:02:21 +0500

    MESSAGE: Ready

--- Program.cs
+++ Program.cs
@@ -7,15 +7,17 @@ namespace MohirDevVazifa
         static void Main(string[] args)
         {
             Console.WriteLine("MohirDev Vazifa Basic \"2\"");
-
+            Console.WriteLine("_____________________Task #1_____________________");
             Console.WriteLine("Welcome to the Calculator program!");
             Calculator calculator = new Calculator();
             calculator.EnterValueAndCalculate();
 
+            Console.WriteLine("\n_____________________Task #2_____________________");
             Console.WriteLine("Welcome to the Sum of Numbers program!");
             SumOfNumbers sumCalculator = new SumOfNumbers();
             Console.WriteLine($"Sum of Numbers is: {sumCalculator.CalculateSum()}");
 
+            Console.WriteLine("\n_____________________Task #3_____________________");
             Console.WriteLine("Welcome to the Parity Check program!");
             ParityCheck parityChecker = new ParityCheck();
             parityChecker.CheckParity();
